df418a9ab78138ec70e4e96d5c5564be5b8f3949
koo <kookh0987@gmail.com>
2017-03-19 14:38:51 +0900

    [solve] 1 problem on Category load

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 import Editor from './Editor';
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import {updateArticle} from './actions/Article'
+import {updateArticle, loadArticles} from './actions/Article'
 import Search from './Search'
 import SearchCategory from './SearchCategory'
+
 
 
 /*
@@ -21,6 +22,8 @@ class App extends Component {
     if(article){
       const {dispatch} = this.props;
       dispatch(updateArticle(article));
+
+      dispatch(loadArticles());
       this.forceUpdate();
     }
   }
